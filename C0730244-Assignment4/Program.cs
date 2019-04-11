cacf8f29a4cb8994b910e5b2346fe0d19ecc4d1d
visualstudio <visualstudio@230-PC38>
2019-04-11 16:42:24 -0400

    section c

--- C0730244-Assignment4/Program.cs
+++ C0730244-Assignment4/Program.cs
@@ -15,16 +15,47 @@ namespace C0730244_Assignment4
         {
            Program p = new Program();
            p.Beowulf = new ArrayList();
+            p.Run();
+
         }
         public void Run()
         {
             this.ReadTextFiles();
+            this.ProcessArrayList();
+        }
+        public void ProcessArrayList()
+        {
+            int LineNumber = 0;
+            foreach(var line in Beowulf)
+            {
+                
+
+                if(ContainsWord( line.ToString().ToLower(), "sea")&& ContainsWord(line.ToString().ToLower(), "fare"))
+                {
+                    Console.WriteLine("line number is {0}", LineNumber);
+                    Console.WriteLine(line);
+                    LineNumber++;
+                }
+            }
+            Console.WriteLine(LineNumber);
+        }
+        public bool ContainsWord(string line, string word)
+        {
+            // to do : write algorith to detect the line contain a word
+            if(line.Contains(word)== true)
+            {
+                //Console.WriteLine("Word Found");
+                return true;
+            }
+            return false;
         }
         public void ReadTextFiles()
         {
-           // Read file using streamReader. Reads file line by line
-           using (StreamReader file = new StreamReader("U:/Users/730244/New folder/C0730244-Assignment4"))
+           
+            // Read file using streamReader. Reads file line by line
+           using (StreamReader file = new StreamReader("U:/Users/730244/New folder/Beowulf.txt"))
            {
+              int words = 1;
               int counter = 0;
               string ln;
 
@@ -32,10 +63,12 @@ namespace C0730244_Assignment4
               {
                     Console.WriteLine(ln);
                     Beowulf.Add(ln);
+                    counter++;
               }
               file.Close();
+              Console.WriteLine("=======================================================================");
               Console.WriteLine($"File has {counter} lines.");
-              Console.ReadLine();
+              Console.ReadLine(); 
            }
         }
 
