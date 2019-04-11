1de98715666c8e121a9eb2fb64d75b4aadfc4d77
visualstudio <visualstudio@230-PC38>
2019-04-11 14:33:28 -0400

    third commit

--- C0730244-Assignment4/Program.cs
+++ C0730244-Assignment4/Program.cs
@@ -10,35 +10,46 @@ namespace C0730244_Assignment4
 {
     class Program
     {
-            ArrayList Beowulf;
-
-            static void Main(string[] arge)
-            {
-                Program p = new Program();
-                p.Beowulf = new ArrayList();
-            }
-
-            public void Run()
-            {
+        ArrayList Beowulf;
+        static void Main(string[] arge)
+        {
+           Program p = new Program();
+           p.Beowulf = new ArrayList();
+        }
+        public void Run()
+        {
             this.ReadTextFiles();
-            }
-            public void ReadTextFiles()
-            {
-            // Read file using streamReader. Reads file line by line
-            using (StreamReader file = new StreamReader("U:/Users/730244/New folder/C0730244-Assignment4"))
-                {
-                int counter = 0;
-                string ln;
+        }
+        public void ReadTextFiles()
+        {
+           // Read file using streamReader. Reads file line by line
+           using (StreamReader file = new StreamReader("U:/Users/730244/New folder/C0730244-Assignment4"))
+           {
+              int counter = 0;
+              string ln;
 
-                while((ln= file.ReadLine()) !=null)
-                    {
+              while((ln= file.ReadLine()) !=null)
+              {
                     Console.WriteLine(ln);
                     Beowulf.Add(ln);
-                    }
-                file.Close();
-                Console.WriteLine($"File has {counter} lines.");
-                Console.ReadLine();
-                }
+              }
+              file.Close();
+              Console.WriteLine($"File has {counter} lines.");
+              Console.ReadLine();
+           }
+        }
+
+        public int FindNumberOfBlankSpaces(string line)
+        {
+            int countletters = 0;
+            int countSpaces = 0;
+             
+            foreach (char c in line)
+            {
+                if(char.IsLetter(c)) { countletters++; }
+                if(char.IsWhiteSpace(c)) { countSpaces++; }
             }
+            return countSpaces;
         }
     }
+}
