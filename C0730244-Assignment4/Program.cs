0d8859731c74e3acab02f70f9a4f00dca105d658
visualstudio <visualstudio@230-PC38>
2019-04-11 14:13:19 -0400

    second commit

--- C0730244-Assignment4/Program.cs
+++ C0730244-Assignment4/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,35 @@ namespace C0730244_Assignment4
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-           ArrayList Beowulf;
+            ArrayList Beowulf;
+
+            static void Main(string[] arge)
+            {
+                Program p = new Program();
+                p.Beowulf = new ArrayList();
+            }
+
+            public void Run()
+            {
+            this.ReadTextFiles();
+            }
+            public void ReadTextFiles()
+            {
+            // Read file using streamReader. Reads file line by line
+            using (StreamReader file = new StreamReader("U:/Users/730244/New folder/C0730244-Assignment4"))
+                {
+                int counter = 0;
+                string ln;
+
+                while((ln= file.ReadLine()) !=null)
+                    {
+                    Console.WriteLine(ln);
+                    Beowulf.Add(ln);
+                    }
+                file.Close();
+                Console.WriteLine($"File has {counter} lines.");
+                Console.ReadLine();
+                }
+            }
         }
     }
-}
